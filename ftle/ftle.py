9fbe66afe50a3c95a105f951881e7d0df24d3214
danoherl <126787509+danoherl@users.noreply.github.com>
2025-08-12 22:39:49 +0100

    Skeleton structure for ftle.py

---
+++ ftle/ftle.py
@@ -0,0 +1,40 @@
+# Class to create FTLE field.
+"""
+Ideas from:
+Shadden, Shawn & Lekien, Francois & Marsden, Jerrold. (2005).
+            Definition and properties of Lagrangian coherent structures from
+            finit-time Lyapunov exponents in two-dimensional aperiodic flows.
+            Physica D. 212. 271-304. 10.1016/j.physd.2005.10.007.
+"""
+
+# Functions included:
+class FTLE:
+    """
+    Class to undertake Finite Time Lyapunov Exponent analysis.
+    """
+    def __init__(self, t_0, time):
+        """
+        Initialise setup, in order to extract FTLE.
+        Args:
+            - t_0 (int): The initial time of the simulation.
+            - time (int): T (once converted into seconds), where [t0, t0 + T]
+                is the domain of integration. 
+        """
+        self.t_0 = t_0
+        self.time = time 
+        
+
+
+    def time_to_T(self):
+        """
+        Converts integration time to seconds.
+        """
+        
+
+    def get_ftle(self):
+        """
+        Calculates a 2D FTLE field. 
+        """
+        
+
+
